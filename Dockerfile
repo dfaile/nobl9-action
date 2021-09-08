b5c676382712b2757f60f96dc8b7cf5bcdf7c10d
catalin.craciun <craciunctln@gmail.com>
2021-09-08 18:11:12 +0300

    Using args for params

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,6 @@
 FROM alpine:3.10
 
-COPY debug.sh /debug.sh
-RUN chmod +x /debug.sh
-RUN ./debug.sh
-
-ADD "https://github.com/nobl9/sloctl/releases/download/${SLOCTL_VERSION}/sloctl-linux-${SLOCTL_VERSION}.zip" /usr/local/bin
-
-
 COPY entrypoint.sh /entrypoint.sh
-RUN echo 'sloctl apply -f $GITHUB_WORKSPACE/${SLOCTL_YML}' >> /entrypoint.sh
 RUN chmod +x /entrypoint.sh
-
-COPY config.toml ~/.config/nobl9/config.toml
-RUN echo '    clientId = "${CLIENT_ID}"' >> ~/.config/nobl9/config.toml
-RUN echo '    clientSecret = "${CLIENT_SECRET}"' >> ~/.config/nobl9/config.toml
-RUN echo '    accessToken = "${ACCESS_TOKEN}"' >> ~/.config/nobl9/config.toml
-RUN echo '    project = "${PROJECT}"' >> ~/.config/nobl9/config.toml
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,24 @@
 #!/bin/sh
 
+SLOCTL_VERSION=$1
+CLIENT_ID=$2
+CLIENT_SECRET=$3
+ACCESS_TOKEN=$4
+PROJECT=$5
+SLOCTL_YML=$6
+
+wget https://github.com/nobl9/sloctl/releases/download/"${SLOCTL_VERSION}"/sloctl-linux-"${SLOCTL_VERSION}".zip
+unzip sloctl-linux-"${SLOCTL_VERSION}".zip
+sudo install -o root -g root -m 0755 ./sloctl /usr/local/bin/sloctl
+mkdir -p ~/.config/nobl9
+touch ~/.config/nobl9/config.toml
+
+echo 'defaultContext = "default"' >> ~/.config/nobl9/config.toml
+echo '[Contexts]' >> ~/.config/nobl9/config.toml
+echo '  [Contexts.default]' >> ~/.config/nobl9/config.toml
+echo "    clientId = \"${CLIENT_ID}\"" >> ~/.config/nobl9/config.toml
+echo "    clientSecret = \"${CLIENT_SECRET}\"" >> ~/.config/nobl9/config.toml
+echo "    accessToken = \"${ACCESS_TOKEN}\"" >> ~/.config/nobl9/config.toml
+echo "    project = \"${PROJECT}\"" >> ~/.config/nobl9/config.toml
+
+sloctl apply -f "$GITHUB_WORKSPACE"/"$SLOCTL_YML"
